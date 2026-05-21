9a154e9b1f7feb3cb131995e8ee6193105e5d31d
NasrulDanial <2025386309@student.uitm.edu.my>
2026-05-22 02:32:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/easyzakatpay/AboutActivity.java
@@ -0,0 +1,45 @@
+package com.example.easyzakatpay;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    TextView txtGithub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.aboutToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtGithub = findViewById(R.id.txtGithub);
+
+        txtGithub.setOnClickListener(view -> {
+
+            String url =
+                    "https://github.com/yourusername/easyzakatpay";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/easyzakatpay/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.easyzakatpay;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.net.Uri;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtWeight, edtPrice;
+    RadioGroup radioGroupType;
+    RadioButton radioKeep, radioWear;
+    Button btnCalculate, btnReset;
+    TextView txtTotalValue, txtZakatWeight, txtZakatPayable, txtTotalZakat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // CONNECT XML COMPONENTS
+
+        edtWeight = findViewById(R.id.edtWeight);
+        edtPrice = findViewById(R.id.edtPrice);
+
+        radioGroupType = findViewById(R.id.radioGroupType);
+        radioKeep = findViewById(R.id.radioKeep);
+        radioWear = findViewById(R.id.radioWear);
+
+        btnCalculate = findViewById(R.id.btnCalculate);
+        btnReset = findViewById(R.id.btnReset);
+        Animation buttonAnim = AnimationUtils.loadAnimation(this, R.anim.button_animation);
+
+        txtTotalValue = findViewById(R.id.txtTotalValue);
+        txtZakatWeight = findViewById(R.id.txtZakatWeight);
+        txtZakatPayable = findViewById(R.id.txtZakatPayable);
+        txtTotalZakat = findViewById(R.id.txtTotalZakat);
+
+        // CALCULATE BUTTON
+        btnCalculate.startAnimation(buttonAnim);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // VALIDATION
+
+                if (edtWeight.getText().toString().isEmpty()) {
+                    edtWeight.setError("Please enter gold weight");
+                    edtWeight.requestFocus();
+                    return;
+                }
+
+                if (edtPrice.getText().toString().isEmpty()) {
+                    edtPrice.setError("Please enter gold price");
+                    edtPrice.requestFocus();
+                    return;
+                }
+
+                if (radioGroupType.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(MainActivity.this, "Please select gold type", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // GET VALUES
+                double weight = Double.parseDouble(edtWeight.getText().toString());
+                double price = Double.parseDouble(edtPrice.getText().toString());
+                double uruf;
+                double zakatWeight;
+                double zakatPayable;
+                double totalValue;
+                double totalZakat;
+
+                // CHECK GOLD TYPE
+                if (radioKeep.isChecked()) {
+                    uruf = 85;
+                } else {
+                    uruf = 200;
+                }
+
+                // CALCULATIONS
+                totalValue = weight * price;
+                zakatWeight = weight - uruf;
+                if (zakatWeight < 0) {
+                    zakatWeight = 0;
+                }
+
+                zakatPayable = zakatWeight * price;
+                totalZakat = zakatPayable * 0.025;
+
+                // DISPLAY OUTPUT
+                txtTotalValue.setText("Total Gold Value: RM " + String.format("%.2f", totalValue));
+                txtZakatWeight.setText("Zakat Weight: " + String.format("%.2f", zakatWeight) + " gram");
+                txtZakatPayable.setText("Zakat Payable: RM " + String.format("%.2f", zakatPayable));
+                txtTotalZakat.setText("Total Zakat: RM " + String.format("%.2f", totalZakat));
+
+                // SUCCESS MESSAGE
+                Toast.makeText(MainActivity.this, "Zakat calculated successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // RESET BUTTON
+        btnReset.startAnimation(buttonAnim);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edtWeight.setText("");
+                edtPrice.setText("");
+
+                radioGroupType.clearCheck();
+
+                txtTotalValue.setText("Total Gold Value: RM 0.00");
+                txtZakatWeight.setText("Zakat Weight: 0 gram");
+                txtZakatPayable.setText("Zakat Payable: RM 0.00");
+                txtTotalZakat.setText("Total Zakat: RM 0.00");
+
+                Toast.makeText(MainActivity.this, "All fields reset", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuAbout) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menuShare) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out EasyZakat Pay:\nhttps://github.com/nasrul/easyzakatpay");
+            startActivity(Intent.createChooser(shareIntent, "Share via"));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
